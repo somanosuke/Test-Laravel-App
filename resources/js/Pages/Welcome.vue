63515e268049189d7f1bd71358017f04ee362531
Soma Iwasaki <somanosuke.suke@gmail.com>
2025-09-24 11:57:41 +0900

    first

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+Hello,World
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', []);
+});
+Route::get('/app', function () {
+    return Inertia::render('App', []);
 });
